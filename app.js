2d711067ca01462076079db69904a7925873027a
ashraf <ashrafeid@outlook.com>
2023-04-28 20:32:09 +0300

    remove redirection

--- app.js
+++ app.js
@@ -51,12 +51,18 @@ if (process.env.NODE_ENV === "development") {
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "build")));
+// app.use(express.static(join(__dirname, "build")));
 
 app.use((req, res, next) => {
   req.io = io;
   next();
 });
+app.get("/", (req, res) => {
+  res.send("API is running");
+}
+);
+
+
 
 app.use("/profile", profileRouter);
 app.use("/auth", authRouter);
@@ -65,20 +71,20 @@ app.use("/notification", notificationRouter);
 app.use("/conversations", protect, conversationRouter);
 app.use("/messages", protect, messageRouter);
 app.use("/request", requestRouter);
-app.get('*', (req, res) => {
-  res.redirect('/')
-})
+// app.get('*', (req, res) => {
+//   res.redirect('/')
+// })
 
 
 // if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/build")));
+//   app.use(express.static(path.join(__dirname, "build")));
 
 //   app.get("*", (req, res) =>
 //     res.sendFile(path.resolve(__dirname), "build", "index.html")
 //   );
 // } else {
 //   app.get("/", (req, res) => {
-//     res.send("API is running");
+    // res.send("API is running");
 //   });
 // }
 
