c583fcf3d3f8d64d4f6af44738a7b19fc8b3dd80
ashraf <ashrafeid@outlook.com>
2023-04-28 19:16:48 +0300

    modify packages

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
+    // useFindAndModify: false,
   });
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
